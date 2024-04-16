fad88ab26f21629f376565c59de17af55f1f0eaa
26CheMauHanh <chemauhanh@gmail.com>
2024-04-16 14:44:15 +0700

    add file

---
+++ SeleniumLevel1/src/Common/Constant/Constant.java
@@ -0,0 +1,30 @@
+package Common.Constant;
+
+import org.openqa.selenium.WebDriver;
+
+public class Constant {
+  public static WebDriver WEBDRIVER ;
+  public static final String RAILWAY_URL= "http://railwayb1.somee.com";
+  public static final String BOOK_TICKET_URL= "http://railwayb1.somee.com/Page/BookTicketPage.cshtml";
+  public static final String USERNAME= "chemauhanh@gmail.com";
+  public static final String PASSWORD = "Hanhtoi263";
+
+
+
+
+  public static final String USERNAME_NEW= "nguyenvana@gmail.com";
+  public static final String PASSWORD_NEW = "NgocHuy1234";
+  public static final String PASSWORD_CHANGE = "Hanhtoi263";
+
+  public static final String CONFIRM_PASSWORD = "Hanhtoi263";
+
+  public static final String PID = "12342009";
+
+
+  public static final String INVALID_PASSWORD = "Hanhtoi1234";
+
+
+
+
+
+}
